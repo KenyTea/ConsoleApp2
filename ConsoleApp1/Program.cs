d783772ab56c5761c4aab6e280f27c40ea0a5acc
Алексей Васильев <Vassilyevalexey@gmail.com>
2018-02-13 19:11:46 +0600

    HW

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*1.	Составить программу вывода на экран в одну строку трех любых чисел с двумя пробелами между ними.*/
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            int a = 5;
+            int b = 10;
+            int c = 15;
+            Console.WriteLine(a ); 
+        }
+    }
+}
