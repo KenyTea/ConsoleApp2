e74c06cf521dadec4f90cdbbb7111fc72ed7933e
Alex <vassilyevalexey@gmail.com>
2018-02-14 09:41:18 +0600

    дз пример 6

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,29 +10,59 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             /*1.	Составить программу вывода на экран в одну строку трех любых чисел с двумя пробелами между ними.*/
-
-            int a = 5;
-            int b = 10;
-            int c = 15;
-            Console.Write(a ); Console.Write(' '); Console.Write(b); Console.Write(' '); Console.Write(c);
-            Console.WriteLine();
-            Console.WriteLine();
+            //int a = 5;
+            //int b = 10;
+            //int c = 15;
+            //Console.Write(a ); Console.Write(' '); Console.Write(b); Console.Write(' '); Console.Write(c);
+            //Console.WriteLine();
+            //Console.WriteLine();
 
             /*2.	Вывести на экран числа 5, 10 и 21 одно под другим.*/
-            int d = 5;
-            int e = 10;
-            int f = 21;
-            Console.WriteLine(d); Console.WriteLine(e); Console.WriteLine(f);
-            Console.WriteLine();
+            //int d = 5;
+            //int e = 10;
+            //int f = 21;
+            //Console.WriteLine(d); Console.WriteLine(e); Console.WriteLine(f);
+            //Console.WriteLine();
 
             /*3.	Дано расстояние в сантиметрах. Найти число полных метров в нем.*/
-            Console.WriteLine("Enter number");
-            float temp = float.Parse(Console.ReadLine());
-            temp = temp / 1000;
-            Console.Write(temp); Console.WriteLine(" m.");
-            Console.WriteLine();
+            //Console.WriteLine("Enter number");
+            //float temp = float.Parse(Console.ReadLine());
+            //temp = temp / 1000;
+            //Console.Write(temp); Console.WriteLine(" m.");
+            //Console.WriteLine();
 
             /*4.	С некоторого момента прошло 234 дня. Сколько полных недель прошло за этот период?*/
+            //int days = 234;
+            //int weeks;
+            //weeks = days / 7;
+            //Console.Write(weeks); Console.WriteLine(" weeks");
+
+            /*5.	Дано двузначное число. Найти:
+                        a.	число десятков в нем;
+                        b.	число единиц в нем;
+                        c.	сумму его цифр;
+                        d.	произведение его цифр
+                        */
+            //int value = 25;
+            //int doz = value  / 10;
+            //int unit = value % 10;
+            //int sum = doz + unit;
+            //int multi = doz * unit;
+            //Console.Write("dez - "); Console.WriteLine(doz);
+            //Console.Write("unit - "); Console.WriteLine(unit);
+            //Console.Write("sum - "); Console.WriteLine(sum);
+            //Console.Write("multiplication - "); Console.WriteLine(multi);
+
+            /*6.	Вычислить значение логического выражения при следующих значениях логических величин
+             * А, В и С: А = Истина, В = Ложь, С = Ложь:
+                          a.	А или В; 
+                          b.	А и В; 
+                          c.	В или С.
+                          */
+            bool A = true;
+            bool B = false;
+            bool C = false;
+
 
 
         }
