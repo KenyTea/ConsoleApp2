d7c1b2c347e4d1cb08f6c783c7da7a1de72704c9
Алексей Васильев <Vassilyevalexey@gmail.com>
2018-02-21 20:38:23 +0600

    Classes

---
+++ OOP_21.02.18/Classes/Person.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_21._02._18.Classes
+{
+    class Person
+    {
+    }
+}
---
+++ OOP_21.02.18/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OOP_21._02._18.Classes;
+
+namespace OOP_21._02._18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+
+        }
+    }
+}
